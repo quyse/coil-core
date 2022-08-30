f3bdbfce33fd942ef9098d0ca303746c0d27a73b
Alexander Bich <code@quyse.net>
2022-08-30 19:13:44 +0600

    some debug printing

---
+++ src/debug.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "math_debug.hpp"
---
+++ src/math_debug.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "math.hpp"
+#include <ostream>
+
+namespace Coil
+{
+  template <typename T, size_t n>
+  std::ostream& operator<<(std::ostream& s, xvec<T, n> const& a)
+  {
+    s << "xvec<" << typeid(T).name() << ", " << n << ">{ ";
+    for(size_t i = 0; i < n; ++i)
+    {
+      if(i) s << ", ";
+      s << a(i);
+    }
+    return s << " }";
+  }
+
+  template <typename T, size_t n, size_t m>
+  std::ostream& operator<<(std::ostream& s, xmat<T, n, m> const& a)
+  {
+    s << "xmat<" << typeid(T).name() << ", " << n << ", " << m << ">{\n";
+    for(size_t i = 0; i < n; ++i)
+    {
+      for(size_t j = 0; j < m; ++j)
+        s << a(i, j) << ",\t";
+      s << '\n';
+    }
+    return s << "}\n";
+  }
+}
