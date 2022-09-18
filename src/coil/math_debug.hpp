880aad0e6b2054e20d14ac15c2de657db7943772
Alexander Bich <code@quyse.net>
2022-09-18 18:37:05 +0600

    debug math: output vectors with any alignment

--- src/coil/math_debug.hpp
+++ src/coil/math_debug.hpp
@@ -5,8 +5,8 @@
 
 namespace Coil
 {
-  template <typename T, size_t n>
-  std::ostream& operator<<(std::ostream& s, xvec<T, n> const& a)
+  template <typename T, size_t n, size_t alignment>
+  std::ostream& operator<<(std::ostream& s, xvec<T, n, alignment> const& a)
   {
     s << "xvec<" << typeid(T).name() << ", " << n << ">{ ";
     for(size_t i = 0; i < n; ++i)
