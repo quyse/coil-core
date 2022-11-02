5e3ecc40a5f789cd837db69fe56c7429e43bf4ef
Alexander Bich <code@quyse.net>
2022-11-03 02:07:04 +0600

    build system fixes

--- coil-core.nix
+++ coil-core.nix
@@ -11,23 +11,9 @@
 , libpng
 , freetype
 , harfbuzz
-}: let
-  sources = lib.pipe ./src/coil [
-    builtins.readDir
-    (lib.filterAttrs (file: type: lib.hasSuffix ".cpp" file && type == "regular"))
-    lib.attrNames
-  ];
-  buildfile = writeText "coil-core.ninja" ''
-    rule cxx
-      command = clang++ -c -std=c++20 -o $out $in
-    rule ar
-      command = ar -r $out $in
-    ${lib.concatStrings (map (source: ''
-      build ${source}.o: cxx ${source}
-    '') sources)}
-    build libcoilcore.a: ar ${lib.concatStringsSep " " (map (source: "${source}.o") sources)}
-  '';
-in stdenv.mkDerivation {
+}:
+
+stdenv.mkDerivation {
   name = "coil-core";
   src = ./src/coil;
   nativeBuildInputs = [
@@ -48,8 +34,4 @@ in stdenv.mkDerivation {
     "-DCMAKE_CXX_COMPILER=clang++"
     "-DCMAKE_C_COMPILER=clang"
   ];
-  installPhase = ''
-    mkdir $out
-    mv *.a $out/
-  '';
 }
