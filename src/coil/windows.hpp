d830d6f80492e1c6eaa769c533b0a1f2b8e613da
Alexander Bich <code@quyse.net>
2022-12-25 18:03:23 +0600

    windows: various utils

--- src/coil/windows.hpp
+++ src/coil/windows.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "base.hpp"
+#include <thread>
 
 #if !defined(___COIL_PLATFORM_WINDOWS)
   #error not windows
@@ -14,3 +15,199 @@
 #endif
 
 #include <windows.h>
+// auto-linking COM
+#include <initguid.h>
+
+// undef unpleasant macros
+#undef GetMessage
+
+namespace Coil
+{
+  class Handle
+  {
+  public:
+    Handle(HANDLE handle = nullptr);
+    Handle(Handle const&) = delete;
+    Handle(Handle&& other);
+    ~Handle();
+
+    Handle& operator=(Handle const&) = delete;
+    Handle& operator=(Handle&& other);
+
+    operator HANDLE() const;
+
+  private:
+    HANDLE _handle = nullptr;
+  };
+
+  // CoInitializeEx thread initializer
+  class ComThread
+  {
+  public:
+    ComThread(bool multiThreaded);
+    ~ComThread();
+  };
+
+  // throw exception on unsuccessful HRESULT
+  void CheckHResult(HRESULT hr, char const* message);
+
+  // pointer to COM object
+  template <typename T>
+  class ComPtr
+  {
+  public:
+    ComPtr(T* object = nullptr)
+    : _object(object) {}
+
+    ComPtr(ComPtr const& other)
+    : _object(other._object)
+    {
+      if(_object)
+      {
+        _object->AddRef();
+      }
+    }
+
+    ComPtr(ComPtr&& other)
+    {
+      std::swap(_object, other._object);
+    }
+
+    ~ComPtr()
+    {
+      if(_object)
+      {
+        _object->Release();
+      }
+    }
+
+    ComPtr& operator=(ComPtr const& other)
+    {
+      if(_object)
+      {
+        _object->Release();
+      }
+      _object = other._object;
+      if(_object)
+      {
+        _object->AddRef();
+      }
+      return *this;
+    }
+
+    ComPtr& operator=(ComPtr&& other)
+    {
+      std::swap(_object, other._object);
+      return *this;
+    }
+
+    operator T*() const
+    {
+      return _object;
+    }
+    T* operator->() const
+    {
+      return _object;
+    }
+
+    // only for use for COM object-creation functions expecting pointer to pointer
+    // releases current object to not lose it
+    // new object is expected to have single reference
+    T** operator&()
+    {
+      if(_object)
+      {
+        _object->Release();
+        _object = nullptr;
+      }
+      return &_object;
+    }
+
+  private:
+    T* _object = nullptr;
+  };
+
+  // pointer to memory allocated with CoTaskMemAlloc
+  template <typename T>
+  class CoTaskMemPtr
+  {
+  public:
+    CoTaskMemPtr(T* object = nullptr)
+    : _object(object) {}
+    CoTaskMemPtr(CoTaskMemPtr const&) = delete;
+    CoTaskMemPtr(CoTaskMemPtr&& other)
+    {
+      std::swap(_object, other._object);
+    }
+
+    ~CoTaskMemPtr()
+    {
+      if(_object)
+      {
+        CoTaskMemFree(_object);
+      }
+    }
+
+    CoTaskMemPtr& operator=(CoTaskMemPtr const&) = delete;
+    CoTaskMemPtr& operator=(CoTaskMemPtr&& other)
+    {
+      std::swap(_object, other._object);
+      return *this;
+    }
+
+    T& operator*() const
+    {
+      return *_object;
+    }
+
+    operator T*() const
+    {
+      return _object;
+    }
+    T* operator->() const
+    {
+      return _object;
+    }
+
+    // only for use for COM allocation functions expecting pointer to pointer
+    // frees current object to not lose it
+    T** operator&()
+    {
+      if(_object)
+      {
+        CoTaskMemFree(_object);
+        _object = nullptr;
+      }
+      return &_object;
+    }
+
+  private:
+    T* _object = nullptr;
+  };
+
+
+  template <typename F>
+  struct UserAPCHandlerBase;
+  template <typename O>
+  struct UserAPCHandlerBase<void (O::*)()>
+  {
+    using Object = O;
+    template <void (Object::*handler)()>
+    static void Handler(ULONG_PTR param)
+    {
+      (((Object*)param)->*handler)();
+    }
+  };
+  template <auto handler>
+  struct UserAPCHandler
+  {
+    using Base = UserAPCHandlerBase<decltype(handler)>;
+    using Object = typename Base::Object;
+    static constexpr auto Handler = &Base::template Handler<handler>;
+
+    static void Queue(std::thread& thread, Object* object)
+    {
+      QueueUserAPC(Handler, thread.native_handle(), (ULONG_PTR)object);
+    }
+  };
+}
