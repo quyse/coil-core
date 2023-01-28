4c4257b4904da09b8db0bdeccdd47a622d868c19
Alexander Bich <code@quyse.net>
2023-01-28 21:19:39 +0600

    localization: use namespaces

--- src/coil/localization_en.hpp
+++ src/coil/localization_en.hpp
@@ -1,43 +1,40 @@
 #pragma once
 
-namespace Coil::Localization
+namespace Coil::Localization::En
 {
-  struct En
+  template <size_t argIndex, typename One, typename Many>
+  class Plural
   {
-    template <size_t argIndex, typename One, typename Many>
-    class Plural
+  private:
+    using CombinedArgs = TextArgHelper::CombinedChunksArgs<ArgText<uint64_t, argIndex>, One, Many>;
+
+    One _one;
+    Many _many;
+
+  public:
+    constexpr Plural(One&& one, Many&& many)
+    : _one(std::move(one)), _many(std::move(many)) {}
+
+    using Args = typename CombinedArgs::Args;
+    static constexpr auto const argsIndices = CombinedArgs::argsIndices;
+
+    VariantText<
+      decltype(CombinedArgs::ResolveChunk(std::declval<One>(), std::declval<Args>())),
+      decltype(CombinedArgs::ResolveChunk(std::declval<Many>(), std::declval<Args>()))
+    > Resolve(Args const& args) const
     {
-    private:
-      using CombinedArgs = TextArgHelper::CombinedChunksArgs<ArgText<uint64_t, argIndex>, One, Many>;
+      uint64_t n = std::get<CombinedArgs::GetArgRealIndex(argIndex)>(args);
 
-      One _one;
-      Many _many;
-
-    public:
-      constexpr Plural(One&& one, Many&& many)
-      : _one(std::move(one)), _many(std::move(many)) {}
-
-      using Args = typename CombinedArgs::Args;
-      static constexpr auto const argsIndices = CombinedArgs::argsIndices;
-
-      VariantText<
-        decltype(CombinedArgs::ResolveChunk(std::declval<One>(), std::declval<Args>())),
-        decltype(CombinedArgs::ResolveChunk(std::declval<Many>(), std::declval<Args>()))
-      > Resolve(Args const& args) const
+      if(n == 1) return
       {
-        uint64_t n = std::get<CombinedArgs::GetArgRealIndex(argIndex)>(args);
-
-        if(n == 1) return
-        {
-          std::in_place_index<0>,
-          CombinedArgs::ResolveChunk(_one, args),
-        };
-        else return
-        {
-          std::in_place_index<1>,
-          CombinedArgs::ResolveChunk(_many, args),
-        };
-      }
-    };
+        std::in_place_index<0>,
+        CombinedArgs::ResolveChunk(_one, args),
+      };
+      else return
+      {
+        std::in_place_index<1>,
+        CombinedArgs::ResolveChunk(_many, args),
+      };
+    }
   };
 }
