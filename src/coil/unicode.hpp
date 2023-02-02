ee2d4aa42aaa879e388a8c596a0f971ff3600af3
Alexander Bich <code@quyse.net>
2023-02-02 17:46:17 +0600

    unicode: UTF-16 to UTF-32 conversion

--- src/coil/unicode.hpp
+++ src/coil/unicode.hpp
@@ -4,12 +4,12 @@
 
 namespace Coil::Unicode
 {
-  // assuming char means UTF-8, char16_t means UTF-16, char32_t means Unicode code points
+  // assuming char means UTF-8, char16_t means UTF-16, char32_t means UTF-32
 
   template <typename From, typename To, std::input_iterator FromIterator>
   class Iterator;
 
-  // converting UTF-8 to Unicode code points
+  // converting UTF-8 to UTF-32
   template <std::input_iterator FromIterator>
   class Iterator<char, char32_t, FromIterator>
   {
@@ -62,7 +62,7 @@ namespace Coil::Unicode
     FromIterator _it;
   };
 
-  // converting Unicode code points to UTF-8
+  // converting UTF-32 to UTF-8
   template <std::input_iterator FromIterator>
   class Iterator<char32_t, char, FromIterator>
   {
@@ -149,7 +149,62 @@ namespace Coil::Unicode
     mutable uint8_t _octet = 0;
   };
 
-  // converting Unicode code points to UTF-16
+  // converting UTF-16 to UTF-32
+  template <std::input_iterator FromIterator>
+  class Iterator<char16_t, char32_t, FromIterator>
+  {
+  public:
+    Iterator(FromIterator const& it)
+    : _it(it) {}
+    Iterator(FromIterator&& it)
+    : _it(std::move(it)) {}
+
+    char32_t operator*() const
+    {
+      uint16_t w0 = *_it;
+      // non-surrogate
+      if(w0 < 0xD800 || w0 > 0xDFFF) return w0;
+
+      // first code point cannot be trailing surrogate
+      if(w0 >= 0xDC00) return 0xFFFD; // return replacement character
+
+      auto it = _it;
+      uint16_t w1 = *++it;
+
+      // second code point must be trailing surrogate
+      if(w1 < 0xDC00 || w1 > 0xDFFF) return 0xFFFD; // return replacement character
+
+      return 0x10000 + ((char32_t)(w0 - 0xD800) << 10) + (char32_t)(w1 - 0xDC00);
+    }
+
+    Iterator& operator++()
+    {
+      // ignoring trailing surrogates, just trying to find next first valid word
+      while((*++_it & 0xFC00) == 0xDC00);
+      return *this;
+    }
+
+    Iterator operator++(int)
+    {
+      Iterator i = *this;
+      ++(*this);
+      return i;
+    }
+
+    friend auto operator<=>(Iterator const&, Iterator const&) = default;
+
+    using difference_type = ptrdiff_t;
+    using value_type = char32_t;
+    using pointer = value_type*;
+    using reference = value_type&;
+    using iterator_category = std::forward_iterator_tag;
+    using iterator_concept = std::forward_iterator_tag;
+
+  private:
+    FromIterator _it;
+  };
+
+  // converting UTF-32 to UTF-16
   template <std::input_iterator FromIterator>
   class Iterator<char32_t, char16_t, FromIterator>
   {
@@ -161,12 +216,12 @@ namespace Coil::Unicode
 
     char16_t operator*() const
     {
-      char32_t c = *_it;
+      uint32_t c = *_it;
       // BMP code point
-      if(c <= 0xFFFF) return (char16_t)c;
+      if(c <= 0xFFFF) return (char16_t)(uint16_t)c;
 
       c -= 0x10000;
-      return _secondSurrogatePair ? 0xDC00 | (c & 0x3FF) : 0xD800 | ((c >> 10) & 0x3FF);
+      return (char16_t)(_secondSurrogatePair ? (0xDC00 | (c & 0x3FF)) : (0xD800 | ((c >> 10) & 0x3FF)));
     }
 
     Iterator& operator++()
