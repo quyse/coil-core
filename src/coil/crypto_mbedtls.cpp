9c561dbb723bc78b70a23d4ada0da1cfafe82906
Alexander Bich <code@quyse.net>
2024-02-29 14:56:52 +0600

    crypto: SHA1 support

--- src/coil/crypto_mbedtls.cpp
+++ src/coil/crypto_mbedtls.cpp
@@ -3,6 +3,30 @@
 
 namespace Coil::Crypto::MbedTLS
 {
+  SHA1::SHA1()
+  {
+    mbedtls_sha1_init(&_context);
+    mbedtls_sha1_starts(&_context);
+  }
+
+  SHA1::~SHA1()
+  {
+    mbedtls_sha1_free(&_context);
+  }
+
+  void SHA1::Feed(Buffer const& buffer)
+  {
+    mbedtls_sha1_update(&_context, (uint8_t const*)buffer.data, buffer.size);
+  }
+
+  SHA1::Hash SHA1::Finish()
+  {
+    Hash hash;
+    mbedtls_sha1_finish(&_context, hash.data());
+    return hash;
+  }
+
+
   SHA256::SHA256()
   {
     mbedtls_sha256_init(&_context);
--- src/coil/crypto_mbedtls.hpp
+++ src/coil/crypto_mbedtls.hpp
@@ -2,10 +2,26 @@
 
 // crypto implementation based on MbedTLS
 
+#include <mbedtls/sha1.h>
 #include <mbedtls/sha256.h>
 
 namespace Coil::Crypto::MbedTLS
 {
+  class SHA1
+  {
+  public:
+    using Hash = std::array<uint8_t, 20>;
+
+    SHA1();
+    ~SHA1();
+
+    void Feed(Buffer const& buffer);
+    Hash Finish();
+
+  private:
+    mbedtls_sha1_context _context;
+  };
+
   class SHA256
   {
   public:
