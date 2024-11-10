529c6fdd565888f6ea60258c68779f22b0b37b9f
Alexander Bich <code@quyse.net>
2024-11-10 21:33:33 +0500

    build: update libdatachannel and libjuice

--- pkgs/libdatachannel.nix
+++ pkgs/libdatachannel.nix
@@ -11,12 +11,12 @@
 
 stdenv.mkDerivation rec {
   pname = "libdatachannel";
-  version = "0.21.2";
+  version = "0.22.2";
 
   src = fetchgit {
     url = "https://github.com/paullouisageneau/libdatachannel.git";
     rev = "v${version}";
-    hash = "sha256-3fax57oaJvOgbTDPCiiUdtsfAGhICfPkuMihawq06SA=";
+    hash = "sha256-ZGxv4JfJ3b0S7zacIHBlBtCbChADCQG1CfxStA1lKKE=";
     fetchSubmodules = false;
   };
 
--- pkgs/libjuice.nix
+++ pkgs/libjuice.nix
@@ -5,12 +5,12 @@
 
 stdenv.mkDerivation rec {
   pname = "libjuice";
-  version = "1.5.4";
+  version = "1.5.7";
 
   src = fetchgit {
     url = "https://github.com/paullouisageneau/libjuice.git";
     rev = "v${version}";
-    hash = "sha256-NV3Bn+4FKkaboYr/vhXD0lhXjQtsTlTWJiphY/sMBXU=";
+    hash = "sha256-niMzFimcb+6FJq9ks7FJ2yrwJ7NnSEfj/bxLaPhdMDk=";
   };
 
   nativeBuildInputs = [
