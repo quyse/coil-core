7dc265195351ec43313637a75dff8b03e36d9164
Alexander Bich <code@quyse.net>
2025-01-19 22:10:15 +0500

    modules: appidentity

--- src/coil/appidentity.cpp
+++ src/coil/appidentity.cpp
@@ -1,28 +1,45 @@
-#include "appidentity.hpp"
+module;
 
-namespace Coil
+#include <string>
+#include <cstdint>
+
+export module coil.core.appidentity;
+
+export namespace Coil
 {
-  AppIdentity::AppIdentity()
-  : name_{"App"}, packageName_{"app"} {}
-
-  std::string& AppIdentity::Name()
+  // global app identity
+  class AppIdentity
   {
-    return name_;
-  }
+  public:
+    AppIdentity()
+    : name_{"App"}, packageName_{"app"} {}
 
-  std::string& AppIdentity::PackageName()
-  {
-    return packageName_;
-  }
+    // display name for app
+    std::string& Name()
+    {
+      return name_;
+    }
+    // package name for use in directory names, etc
+    // suggestion: use reverse-domain notation, i.e. "com.company.app"
+    std::string& PackageName()
+    {
+      return packageName_;
+    }
+    // app version
+    uint32_t& Version()
+    {
+      return version_;
+    }
 
-  uint32_t& AppIdentity::Version()
-  {
-    return version_;
-  }
+    static AppIdentity& GetInstance()
+    {
+      static AppIdentity instance;
+      return instance;
+    }
 
-  AppIdentity& AppIdentity::GetInstance()
-  {
-    static AppIdentity instance;
-    return instance;
-  }
+  private:
+    std::string name_;
+    std::string packageName_;
+    uint32_t version_ = 0;
+  };
 }
