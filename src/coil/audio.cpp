07569985b15e2203ce8a77ad0efd6b05698e4bb2
Alexander Bich <code@quyse.net>
2025-01-21 03:13:47 +0500

    modules: audio

--- src/coil/audio.cpp
+++ src/coil/audio.cpp
@@ -1,7 +1,19 @@
-#include "audio.hpp"
+module;
 
-namespace Coil
+#include <string_view>
+
+export module coil.core.audio;
+
+import coil.core.base;
+
+export namespace Coil
 {
+  enum class AudioFormatChannels
+  {
+    Mono,
+    Stereo,
+  };
+
   int32_t GetAudioFormatChannelsCount(AudioFormatChannels channels)
   {
     switch(channels)
@@ -25,4 +37,66 @@ namespace Coil
       throw Exception("unknown audio channels count");
     }
   }
+
+  // float is always used for samples
+  using AudioSample = float;
+
+  // audio format
+  struct AudioFormat
+  {
+    // recommended sampling rate
+    static constexpr int32_t recommendedSamplingRate = 48000;
+
+    // channels configuration
+    AudioFormatChannels channels;
+    // sampling rate in Hz
+    int32_t samplingRate = recommendedSamplingRate;
+
+    friend auto operator<=>(AudioFormat const&, AudioFormat const&) = default;
+  };
+
+  // audio input stream
+  // all methods must be thread safe
+  class AudioStream
+  {
+  public:
+    virtual ~AudioStream() {}
+
+    // get stream format
+    // must be constant while stream exists
+    virtual AudioFormat GetFormat() const = 0;
+
+    // read some samples
+    // framesCount specifies recommended number of frames,
+    // according to the latency of the device
+    // if stream returns more frames, they will be queued,
+    // if less, the method will be called again
+    // zero frames returned means EOF
+    // buffer is allowed to be valid only until next read
+    virtual Buffer Read(int32_t framesCount) = 0;
+  };
+
+  class AudioDevice
+  {
+  public:
+    // resume or stop playing
+    virtual void SetPlaying(bool playing) = 0;
+  };
+
+  // audio stream factory
+  class AudioStreamSource
+  {
+  public:
+    virtual ~AudioStreamSource() {}
+
+    // create new stream playing audio from start
+    virtual AudioStream& CreateStream(Book& book) = 0;
+  };
+
+  template <>
+  struct AssetTraits<AudioStreamSource*>
+  {
+    static constexpr std::string_view assetTypeName = "audio";
+  };
+  static_assert(IsAsset<AudioStreamSource*>);
 }
--- src/coil/audio_soft.cpp
+++ src/coil/audio_soft.cpp
@@ -1,130 +1,190 @@
-#include "audio_soft.hpp"
-#include <cstring>
+module;
+
+#include <atomic>
+#include <memory>
+#include <mutex>
+#include <unordered_set>
+
+export module coil.core.audio.soft;
+
+import coil.core.audio;
+import coil.core.base;
 
 namespace Coil
 {
-  AudioPausingStream::AudioPausingStream(AudioStream& stream, bool playing)
-  : _stream(stream), _playing(playing), _format(_stream.GetFormat()) {}
-
-  void AudioPausingStream::SetPlaying(bool playing)
+  // stream which can be paused
+  // returns silence while paused
+  class AudioPausingStream : public AudioStream
   {
-    _playing = playing;
-  }
+  public:
+    AudioPausingStream(AudioStream& stream, bool playing = true)
+    : _stream(stream), _playing(playing), _format(_stream.GetFormat()) {}
 
-  AudioFormat AudioPausingStream::GetFormat() const
-  {
-    return _format;
-  }
-
-  Buffer AudioPausingStream::Read(int32_t framesCount)
-  {
-    if(_playing)
+    void SetPlaying(bool playing)
     {
-      return _stream.Read(framesCount);
-    }
-    else
-    {
-      static constexpr int32_t silenceSamplesCount = 0x1000;
-      static constinit AudioSample const silence[silenceSamplesCount] = {};
-      int32_t channelsCount = GetAudioFormatChannelsCount(_format.channels);
-      return { silence, std::min(framesCount, silenceSamplesCount / channelsCount) * channelsCount * sizeof(AudioSample) };
-    }
-  }
-
-  AudioVolumeStream::AudioVolumeStream(AudioStream& stream)
-  : _stream(stream), _format(_stream.GetFormat()) {}
-
-  void AudioVolumeStream::SetVolume(float volume)
-  {
-    _volume = volume;
-  }
-
-  AudioFormat AudioVolumeStream::GetFormat() const
-  {
-    return _format;
-  }
-
-  Buffer AudioVolumeStream::Read(int32_t framesCount)
-  {
-    Buffer buffer = _stream.Read(framesCount);
-    _buffer.resize(buffer.size);
-    for(size_t i = 0; i < buffer.size; i += sizeof(AudioSample))
-      *(AudioSample*)(_buffer.data() + i) = *(AudioSample*)((uint8_t const*)buffer.data + i) * _volume;
-    return _buffer;
-  }
-
-  AudioMixerStream::Player::Player(Book&& book, AudioStream& stream)
-  : _book(std::move(book)), _stream(stream) {}
-
-  AudioMixerStream::AudioMixerStream(AudioFormat const& format)
-  : _format(format) {}
-
-  void AudioMixerStream::Player::Stop()
-  {
-    _playing = false;
-  }
-
-  std::shared_ptr<AudioMixerStream::Player> AudioMixerStream::Play(Book&& book, AudioStream& stream)
-  {
-    if(stream.GetFormat() != _format)
-      throw Exception("wrong stream format to play in audio mixer stream");
-
-    std::shared_ptr<Player> player = std::make_shared<Player>(std::move(book), stream);
-
-    std::scoped_lock lock(_mutex);
-    _players.insert(player);
-
-    return player;
-  }
-
-  AudioFormat AudioMixerStream::GetFormat() const
-  {
-    return _format;
-  }
-
-  Buffer AudioMixerStream::Read(int32_t framesCount)
-  {
-    std::scoped_lock lock(_mutex);
-
-    // read from players which do not have data yet
-    size_t minimumSize = 0;
-    for(auto i = _players.begin(); i != _players.end(); )
-    {
-      auto const& player = *i;
-      // read from stream if it's not cancelled, and there's no existing data
-      if(player->_playing && !player->_buffer.size)
-      {
-        // read data
-        player->_buffer = player->_stream.Read(framesCount);
-      }
-      // if stream is cancelled, or end of sound is reached, remove player
-      if(!player->_playing || !player->_buffer.size)
-      {
-        i = _players.erase(i);
-        continue;
-      }
-
-      if(!minimumSize || minimumSize > player->_buffer.size)
-      {
-        minimumSize = player->_buffer.size;
-      }
-
-      ++i;
+      _playing = playing;
     }
 
-    // sum up all samples up to the minimum
-    _buffer.assign(minimumSize, 0);
-    for(auto const& player : _players)
+    AudioFormat GetFormat() const override
     {
-      auto& playerBuffer = player->_buffer;
-      for(size_t i = 0; i < minimumSize; i += sizeof(AudioSample))
-      {
-        *(AudioSample*)(_buffer.data() + i) += *(AudioSample*)((uint8_t*)playerBuffer.data + i);
-      }
-      playerBuffer.size -= minimumSize;
-      std::memmove(playerBuffer.data, (uint8_t const*)playerBuffer.data + minimumSize, playerBuffer.size);
+      return _format;
     }
 
-    return _buffer;
-  }
+    Buffer Read(int32_t framesCount) override
+    {
+      if(_playing)
+      {
+        return _stream.Read(framesCount);
+      }
+      else
+      {
+        static constexpr int32_t silenceSamplesCount = 0x1000;
+        static constinit AudioSample const silence[silenceSamplesCount] = {};
+        int32_t channelsCount = GetAudioFormatChannelsCount(_format.channels);
+        return { silence, std::min(framesCount, silenceSamplesCount / channelsCount) * channelsCount * sizeof(AudioSample) };
+      }
+    }
+
+  private:
+    AudioStream& _stream;
+    std::atomic<bool> _playing = false;
+    AudioFormat _format;
+  };
+
+  // volume filter
+  class AudioVolumeStream : public AudioStream
+  {
+  public:
+    AudioVolumeStream(AudioStream& stream)
+    : _stream(stream), _format(_stream.GetFormat()) {}
+
+    void SetVolume(float volume)
+    {
+      _volume = volume;
+    }
+
+    AudioFormat GetFormat() const override
+    {
+      return _format;
+    }
+
+    Buffer Read(int32_t framesCount) override
+    {
+      Buffer buffer = _stream.Read(framesCount);
+      _buffer.resize(buffer.size);
+      for(size_t i = 0; i < buffer.size; i += sizeof(AudioSample))
+        *(AudioSample*)(_buffer.data() + i) = *(AudioSample*)((uint8_t const*)buffer.data + i) * _volume;
+      return _buffer;
+    }
+
+  private:
+    AudioStream& _stream;
+    AudioFormat _format;
+    float _volume = 1.0f;
+    std::vector<uint8_t> _buffer;
+  };
+
+  // mixer stream
+  // allows to add streams to mix in real time
+  // streams' formats must be equal to mixer's
+  class AudioMixerStream : public AudioStream
+  {
+  public:
+    class Player
+    {
+    public:
+      Player(Book&& book, AudioStream& stream)
+      : _book(std::move(book)), _stream(stream) {}
+
+      // stop and remove player from mixer
+      // if player is already removed or simply reached end, do nothing
+      void Stop()
+      {
+        _playing = false;
+      }
+
+    private:
+      Book _book;
+      AudioStream& _stream;
+      std::atomic<bool> _playing = true;
+      // access synchronized by mutex in AudioMixerStream
+      Buffer _buffer;
+
+      friend class AudioMixerStream;
+    };
+  public:
+    AudioMixerStream(AudioFormat const& format)
+    : _format(format) {}
+
+    // play input stream from the current moment
+    std::shared_ptr<Player> Play(Book&& book, AudioStream& stream)
+    {
+      if(stream.GetFormat() != _format)
+        throw Exception("wrong stream format to play in audio mixer stream");
+
+      std::shared_ptr<Player> player = std::make_shared<Player>(std::move(book), stream);
+
+      std::scoped_lock lock(_mutex);
+      _players.insert(player);
+
+      return player;
+    }
+
+    AudioFormat GetFormat() const override
+    {
+      return _format;
+    }
+
+    Buffer Read(int32_t framesCount) override
+    {
+      std::scoped_lock lock(_mutex);
+
+      // read from players which do not have data yet
+      size_t minimumSize = 0;
+      for(auto i = _players.begin(); i != _players.end(); )
+      {
+        auto const& player = *i;
+        // read from stream if it's not cancelled, and there's no existing data
+        if(player->_playing && !player->_buffer.size)
+        {
+          // read data
+          player->_buffer = player->_stream.Read(framesCount);
+        }
+        // if stream is cancelled, or end of sound is reached, remove player
+        if(!player->_playing || !player->_buffer.size)
+        {
+          i = _players.erase(i);
+          continue;
+        }
+
+        if(!minimumSize || minimumSize > player->_buffer.size)
+        {
+          minimumSize = player->_buffer.size;
+        }
+
+        ++i;
+      }
+
+      // sum up all samples up to the minimum
+      _buffer.assign(minimumSize, 0);
+      for(auto const& player : _players)
+      {
+        auto& playerBuffer = player->_buffer;
+        for(size_t i = 0; i < minimumSize; i += sizeof(AudioSample))
+        {
+          *(AudioSample*)(_buffer.data() + i) += *(AudioSample*)((uint8_t*)playerBuffer.data + i);
+        }
+        playerBuffer.size -= minimumSize;
+        std::memmove(playerBuffer.data, (uint8_t const*)playerBuffer.data + minimumSize, playerBuffer.size);
+      }
+
+      return _buffer;
+    }
+
+  private:
+    AudioFormat const _format;
+    std::mutex _mutex;
+    std::unordered_set<std::shared_ptr<Player>> _players;
+    std::vector<uint8_t> _buffer;
+  };
 }
