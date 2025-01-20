f6dd9aece55542c88d78d9d5ff24163e59586995
Alexander Bich <code@quyse.net>
2025-01-21 03:19:22 +0500

    modules: video

--- src/coil/video.cpp
+++ src/coil/video.cpp
@@ -1,9 +1,15 @@
-#include "video.hpp"
+module;
 
-namespace
+#include <string_view>
+
+export module coil.core.video;
+
+import coil.core.base;
+import coil.core.image;
+import coil.core.math;
+
+namespace Coil
 {
-  using namespace Coil;
-
   // transforms from/to quantized values
   constexpr mat4x4 transformFromQuantized =
   {
@@ -46,82 +52,136 @@ namespace
   };
 }
 
-namespace Coil
+export namespace Coil
 {
-  VideoFrame::operator bool() const
+  // image format specialized for video frames
+  // only 2D, supports multiple planes of different dimensions
+  struct VideoFrame
   {
-    return format != Format::Unknown;
-  }
+    static constexpr size_t maxPlanesCount = 3;
 
-  RawImage2D<xvec<uint8_t, 3, 0>> VideoFrame::GetImage() const
-  {
-    switch(format)
+    enum class Format
     {
-    case Format::YUV420:
-      {
-        mat4x4 transform;
+      Unknown,
+      YUV420,
+    };
 
-        switch(colorRange)
-        {
-        case ColorRange::Narrow:
-          {
-            static constinit mat4x4 t = transformToQuantized * transformBT709 * transformColorRangeNarrow * transformFromQuantized;
-            transform = t;
-          }
-          break;
-        case ColorRange::Full:
-          {
-            static constinit mat4x4 t = transformToQuantized * transformBT709 * transformColorRangeFull * transformFromQuantized;
-            transform = t;
-          }
-          break;
-        default:
-          throw Exception("unsupported video frame color range for getting image");
-        }
+    enum class ColorRange
+    {
+      Unknown,
+      Narrow, // Y [16..235], UV [16..240]
+      Full, // [0..255]
+    };
 
-        auto clamp = [](float value) -> uint8_t
-        {
-          return (uint8_t)std::min(std::max(value, 0.0f), 255.0f);
-        };
+    Format format = Format::Unknown;
+    ColorRange colorRange = ColorRange::Unknown;
 
-        int32_t const width = planes[0].width;
-        int32_t const height = planes[0].height;
+    struct Plane
+    {
+      void* data = nullptr;
+      int32_t width = 0;
+      int32_t height = 0;
+      int32_t pitch = 0;
+    };
+    Plane planes[maxPlanesCount];
 
-        RawImage2D<xvec<uint8_t, 3, 0>> image({ width, height });
-
-        int32_t i0 = 0, i1 = 0, i2 = 0, ii = 0;
-        for(int32_t i = 0; i < height; ++i)
-        {
-          for(int32_t j = 0; j < width; ++j)
-          {
-            vec4 pixel = transform * vec4
-            {
-              (float)*((uint8_t*)planes[0].data + i0 + j),
-              (float)*((uint8_t*)planes[1].data + i1 + j / 2),
-              (float)*((uint8_t*)planes[2].data + i2 + j / 2),
-              1.0f
-            };
-            image.pixels[ii + j] =
-            {
-              clamp(pixel.x()),
-              clamp(pixel.y()),
-              clamp(pixel.z()),
-            };
-          }
-          i0 += planes[0].pitch;
-          if(i % 2)
-          {
-            i1 += planes[1].pitch;
-            i2 += planes[2].pitch;
-          }
-          ii += image.pitch.y();
-        }
-
-        return std::move(image);
-      }
-      break;
-    default:
-      throw Exception("unsupported video frame format for getting image");
+    operator bool() const
+    {
+      return format != Format::Unknown;
     }
-  }
+
+    RawImage2D<xvec<uint8_t, 3, 0>> GetImage() const
+    {
+      switch(format)
+      {
+      case Format::YUV420:
+        {
+          mat4x4 transform;
+
+          switch(colorRange)
+          {
+          case ColorRange::Narrow:
+            {
+              static constinit mat4x4 t = transformToQuantized * transformBT709 * transformColorRangeNarrow * transformFromQuantized;
+              transform = t;
+            }
+            break;
+          case ColorRange::Full:
+            {
+              static constinit mat4x4 t = transformToQuantized * transformBT709 * transformColorRangeFull * transformFromQuantized;
+              transform = t;
+            }
+            break;
+          default:
+            throw Exception("unsupported video frame color range for getting image");
+          }
+
+          auto clamp = [](float value) -> uint8_t
+          {
+            return (uint8_t)std::min(std::max(value, 0.0f), 255.0f);
+          };
+
+          int32_t const width = planes[0].width;
+          int32_t const height = planes[0].height;
+
+          RawImage2D<xvec<uint8_t, 3, 0>> image({ width, height });
+
+          int32_t i0 = 0, i1 = 0, i2 = 0, ii = 0;
+          for(int32_t i = 0; i < height; ++i)
+          {
+            for(int32_t j = 0; j < width; ++j)
+            {
+              vec4 pixel = transform * vec4
+              {
+                (float)*((uint8_t*)planes[0].data + i0 + j),
+                (float)*((uint8_t*)planes[1].data + i1 + j / 2),
+                (float)*((uint8_t*)planes[2].data + i2 + j / 2),
+                1.0f
+              };
+              image.pixels[ii + j] =
+              {
+                clamp(pixel.x()),
+                clamp(pixel.y()),
+                clamp(pixel.z()),
+              };
+            }
+            i0 += planes[0].pitch;
+            if(i % 2)
+            {
+              i1 += planes[1].pitch;
+              i2 += planes[2].pitch;
+            }
+            ii += image.pitch.y();
+          }
+
+          return std::move(image);
+        }
+        break;
+      default:
+        throw Exception("unsupported video frame format for getting image");
+      }
+    }
+  };
+
+  // video input stream
+  class VideoStream
+  {
+  public:
+    virtual VideoFrame ReadFrame() = 0;
+  };
+
+  class VideoStreamSource
+  {
+  public:
+    virtual ~VideoStreamSource() {}
+
+    virtual VideoStream& CreateStream(Book& book) = 0;
+  };
+
+  template <>
+  struct AssetTraits<VideoStreamSource*>
+  {
+    static constexpr std::string_view assetTypeName = "video";
+  };
+  static_assert(IsAsset<VideoStreamSource*>);
 }
