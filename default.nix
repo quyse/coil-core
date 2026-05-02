ea2ce2d13e8968319b8ae8e945a7db59a0601bb8
Alexander Bich <code@quyse.net>
2026-05-02 23:12:12 +0900

    use makeScope/newScope and make customizations in an stdenv

--- default.nix
+++ default.nix
@@ -6,66 +6,70 @@
 
 lib.makeExtensible (self: with self; {
   # NixOS build
-  nixos-pkgs = pkgs.extend (self: super: with self; {
-    # expose compiler
-    coil.llvmPackages = llvmPackages_21;
-    coil.clang-tools = self.coil.llvmPackages.clang-tools.override { enableLibcxx = true; };
-    coil.clang = self.coil.llvmPackages.clangUseLLVM.override {
-      inherit (self.coil.llvmPackages) bintools; # use lld
-    };
-    # function applying adjustments to a package
-    coil.compile-cpp = pkg: pkg.overrideAttrs (attrs: {
-      nativeBuildInputs = (attrs.nativeBuildInputs or []) ++ [
-        ninja
-        self.coil.clang-tools # for C++ modules to work; must be before clang
-        self.coil.clang
-      ];
-      cmakeFlags = (attrs.cmakeFlags or []) ++ [
-        # force clang
-        "-DCMAKE_CXX_COMPILER=clang++"
-        "-DCMAKE_C_COMPILER=clang"
-        # compile flags
-        "-DCMAKE_CXX_FLAGS=${lib.concatStringsSep " " [
-          # for std::jthread in libc++
-          "-fexperimental-library"
-          # work around linking issue https://github.com/NixOS/nixpkgs/issues/371540
-          "-fno-builtin"
-        ]}"
-        # optimization
-        "-DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON"
-      ];
-      __structuredAttrs = true;
+  nixos-pkgs = pkgs.extend (pkgs: super: with pkgs; {
+    coil = lib.makeScope newScope (self: with self; {
+      # llvm packages taken from nixpkgs
+      llvmPackages = pkgs.llvmPackages_21;
+      clang-tools = llvmPackages.clang-tools.override { enableLibcxx = true; };
+      clang = llvmPackages.clangUseLLVM.override {
+        inherit (llvmPackages) bintools; # use lld
+      };
+      # override stdenv
+      stdenv = stdenvAdapters.overrideMkDerivationArgs (attrs: {
+        nativeBuildInputs = (attrs.nativeBuildInputs or []) ++ [
+          ninja
+          clang-tools # for C++ modules to work; must be before clang
+          clang
+        ];
+        cmakeFlags = (attrs.cmakeFlags or []) ++ [
+          # force clang
+          "-DCMAKE_CXX_COMPILER=clang++"
+          "-DCMAKE_C_COMPILER=clang"
+          # compile flags
+          "-DCMAKE_CXX_FLAGS=${lib.concatStringsSep " " [
+            # for std::jthread in libc++
+            "-fexperimental-library"
+            # work around linking issue https://github.com/NixOS/nixpkgs/issues/371540
+            "-fno-builtin"
+          ]}"
+          # optimization
+          "-DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON"
+        ];
+        __structuredAttrs = true;
+      }) (stdenvAdapters.overrideCC pkgs.stdenv clang);
+
+      core = (callPackage ./coil-core.nix {
+        inherit features;
+      }).overrideAttrs (attrs: {
+        cmakeFlags = (attrs.cmakeFlags or []) ++ [
+          # do not require some libs
+          "-DCOIL_CORE_DONT_REQUIRE_LIBS=${dontRequireLibsList}"
+        ];
+
+      });
+
+
+      libsquish = callPackage ./pkgs/libsquish.nix {};
+
+      libwebm = callPackage ./pkgs/libwebm.nix {};
+      libgav1 = callPackage ./pkgs/libgav1.nix {};
+
+      mbedtls = (super.mbedtls.override {
+        inherit callPackage;
+      }).overrideAttrs (attrs: {
+        postConfigure = (attrs.postConfigure or "") + ''
+          perl scripts/config.pl set MBEDTLS_SSL_DTLS_SRTP
+        '';
+      });
+
+      inherit (llvmPackages) openmp;
+
+      libdatachannel = callPackage ./pkgs/libdatachannel.nix {};
+      plog = callPackage ./pkgs/plog.nix {};
+      libjuice = callPackage ./pkgs/libjuice.nix {};
+      libsrtp = callPackage ./pkgs/libsrtp.nix {};
+      steam-sdk = if coil.toolchain-steam != null then coil.toolchain-steam.sdk else null;
     });
-
-    coil.core = self.coil.compile-cpp ((callPackage ./coil-core.nix {
-      inherit features;
-    }).overrideAttrs (attrs: {
-      cmakeFlags = (attrs.cmakeFlags or []) ++ [
-        # do not require some libs
-        "-DCOIL_CORE_DONT_REQUIRE_LIBS=${dontRequireLibsList}"
-      ];
-    }));
-
-
-    libsquish = self.coil.compile-cpp (callPackage ./pkgs/libsquish.nix {});
-
-    libwebm = self.coil.compile-cpp (callPackage ./pkgs/libwebm.nix {});
-    libgav1 = self.coil.compile-cpp (callPackage ./pkgs/libgav1.nix {});
-
-    mbedtls = self.coil.compile-cpp (super.mbedtls.overrideAttrs (attrs: {
-      postConfigure = (attrs.postConfigure or "") + ''
-        perl scripts/config.pl set MBEDTLS_SSL_DTLS_SRTP
-      '';
-    }));
-
-    inherit (self.coil.llvmPackages) openmp;
-
-    libdatachannel = self.coil.compile-cpp (callPackage ./pkgs/libdatachannel.nix {});
-    plog = self.coil.compile-cpp (callPackage ./pkgs/plog.nix {});
-    libjuice = self.coil.compile-cpp (callPackage ./pkgs/libjuice.nix {});
-    libsrtp = self.coil.compile-cpp (callPackage ./pkgs/libsrtp.nix {});
-
-    steam-sdk = if coil.toolchain-steam != null then coil.toolchain-steam.sdk else null;
   });
   coil-core-nixos = nixos-pkgs.coil.core;
 
@@ -126,7 +130,7 @@ lib.makeExtensible (self: with self; {
 
   # Windows build
   windows-pkgs = import ./pkgs/windows-pkgs.nix {
-    pkgs = nixos-pkgs;
+    pkgs = nixos-pkgs // nixos-pkgs.coil;
     inherit lib coil dontRequireLibsList;
   };
   coil-core-windows = windows-pkgs.coil-core;
